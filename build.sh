afd5004121de76a79546ecb127eaa734f3c50eec
veqox <sonnleitner.luki@gmail.com>
2024-03-05 12:42:39 +0100

    feat: add multi arch build

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 mvn -B package
 cp src/main/docker/Dockerfile target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
-docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
-docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
+docker buildx create  --name multi-arch-builder --platform linux/amd64,linux/arm64
+docker buildx use multi-arch-builder
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest \
+  --push \
+  ./target
